9e8f8943950f03d26ef203cb14699c201c9e0511
samckp <sam.mca.liet@gmail.com>
2020-12-08 11:26:15 +0530

    QRCode reader  added

--- src/main/java/com/qrcode/gen/ReadQRCode.java
+++ src/main/java/com/qrcode/gen/ReadQRCode.java
@@ -1,2 +1,62 @@
-package com.qrcode.gen;public class ReadQRCode {
+package com.qrcode.gen;
+
+// Java code to read the QR code
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.imageio.ImageIO;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+public class ReadQRCode {
+
+    // Function to read the QR file
+    public static String readQRCode(String path, String charset,
+                                Map hashMap)
+            throws FileNotFoundException, IOException,
+            NotFoundException
+    {
+        BinaryBitmap binaryBitmap
+                = new BinaryBitmap(new HybridBinarizer(
+                new BufferedImageLuminanceSource(
+                        ImageIO.read(
+                                new FileInputStream(path)))));
+
+        Result result
+                = new MultiFormatReader().decode(binaryBitmap);
+
+        return result.getText();
+    }
+
+    // Driver code
+    public static void main(String[] args)
+            throws WriterException, IOException,
+            NotFoundException
+    {
+
+        // Path where the QR code is saved
+        String filePath = "src/main/resources/demo.png";
+
+        // Encoding charset
+        String charset = "UTF-8";
+
+        Map<EncodeHintType, ErrorCorrectionLevel> hashMap
+                = new HashMap<EncodeHintType,
+                ErrorCorrectionLevel>();
+
+        System.out.println(
+                "QRCode output: "
+                        + readQRCode(filePath, charset, hashMap));
+    }
 }
